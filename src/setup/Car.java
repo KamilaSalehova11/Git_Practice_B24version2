c99116f2a4b0a13ed4537def6161e0d0e8c90d9c
kamila <kamilasalehova11@gmail.com>
2021-10-04 17:28:31 -0400

    Added WoodenSpoonTest

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,15 +2,14 @@ package setup;
 
 public class Car {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("I want better car");
 
-=======
         // this is our car from james branch
 
         //more text
 
          //more text
->>>>>>> james
+
     }
 }
