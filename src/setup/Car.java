75a04f548263cd69b883e851aca3e7f0c4be1558
kamila <kamilasalehova11@gmail.com>
2021-10-04 16:48:04 -0400

    made car in james branch

---
+++ src/setup/Car.java
@@ -0,0 +1,11 @@
+package setup;
+
+public class Car {
+    public static void main(String[] args) {
+        // this is our car from james branch
+
+        //more text
+
+         //more text
+    }
+}
