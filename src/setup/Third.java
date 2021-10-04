5e917d80a194985dbc2185bef1dec0ba025210c8
kamila <kamilasalehova11@gmail.com>
2021-10-04 14:47:15 -0400

    Adding farm to file

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -4,6 +4,7 @@ public class Third {
     public static void main(String[] args) {
         System.out.println("Namaste");
         System.out.println("Zoo");
+        System.out.println("Farm");
 
     }
 }
