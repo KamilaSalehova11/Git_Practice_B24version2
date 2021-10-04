6aad3b5619a48f5149e6084245f91734c1c416af
kamila <kamilasalehova11@gmail.com>
2021-10-04 18:19:24 -0400

    Added Salute World

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -3,5 +3,7 @@ package tests;
 public class HelloWorldTest {
     public static void main(String[] args) {
         System.out.println("Salute World");
+
+        System.out.println("One More Salute World");
     }
 }
